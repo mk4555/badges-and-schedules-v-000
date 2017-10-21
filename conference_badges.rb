3dc7099a313040554955caf2670347c3b051bfb0
Min Kim <mk4555@nyu.edu>
2017-10-21 17:25:24 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,11 @@
 # Write your code here.
 def badge_maker(name)
-  "Hello, my name is #{name}"
+  "Hello, my name is #{name}."
 end
 def batch_badge_creator(attendees)
   badge = []
   attendees.each do |a|
-    badge.push("Hello, my name is #{a}")
+    badge.push("Hello, my name is #{a}.")
   end
   badge
 end
