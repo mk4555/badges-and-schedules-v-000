999617817f3502435195f26017db2e28ffbcf8db
Min Kim <mk4555@nyu.edu>
2017-10-21 19:03:41 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,29 +1,20 @@
 # Write your code here.
-list = []
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(attendees)
-  counter = 0
-  while counter < attendees.size
-    attendees[counter] = "Hello, my name is #{attendees[counter]}."
-    counter += 1
+  list = []
+  attendees.each do |client|
+    list.push(client)
   end
-  attendees
+  list
 end
 
 def assign_rooms(arr)
-  counter = 0
-  while counter < arr.size
-    arr[counter] = "Hello, #{attendees[counter]}! You'll be assigned to room #{counter + 1}!"
-    counter += 1
-  end
-  arr
+
 end
 
 def printer(attendees)
-  attendees.each do |a|
-    puts "#{a}"
-  end
+
 end
