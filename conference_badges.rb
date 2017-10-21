0432eff2d9bbc046f62b5a4a16ef1e5e929e4855
Min Kim <mk4555@nyu.edu>
2017-10-21 17:27:31 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,10 +2,18 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
 def batch_badge_creator(attendees)
   badge = []
   attendees.each do |a|
     badge.push("Hello, my name is #{a}.")
   end
   badge
+end
+
+def assign_rooms(arr)
+  counter = 1
+  arr.each do |person|
+    "Hello, #{person}! You'll be assigned to room #{counter}"
+    counter += 1
 end
