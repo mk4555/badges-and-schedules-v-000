47af66ee1513621065a9b79c926e833be2115cd5
Min Kim <mk4555@nyu.edu>
2017-10-21 17:25:10 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,4 +2,9 @@
 def badge_maker(name)
   "Hello, my name is #{name}"
 end
-
+def batch_badge_creator(attendees)
+  badge = []
+  attendees.each do |a|
+    badge.push("Hello, my name is #{a}")
+  end
+end
